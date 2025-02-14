406650620e562fbafb88b417b8b8fc957489f8d1
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-02-14 17:16:33 -0500

    Implemented basic regex character classes #19

--- utils/regexPatterns.js
+++ utils/regexPatterns.js
@@ -1,6 +1,12 @@
 export const regexData = {
     characterClasses: {
         digit: "/\d",
-        word: "/\w"
+        word: "/\w",
+        letter: "/[a-zA-Z]",
+        whitespace: "/\s",
+        anyCharacter: "/.",
+        symbol: "/\W",
+        hexadecimal: "/[0-9A-Fa-f]",
+        octal: "/[0-7]"  
     },
 };
